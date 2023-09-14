436ac3cba883c85a3391c4e74333577911e41710
eunhyekim-grace <79469037+eunhyekim-grace@users.noreply.github.com>
2023-09-14 09:55:04 +0900

    Update 폰켓몬.py

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -1,6 +1,6 @@
 def solution(nums):
     leng = int(len(nums)/2)
-    unique = set(nums)
+    unique = set(nums) #nums에서 겹치는 것 빼고 동일한 것만 return
     if len(unique)> leng:
         return leng
     else:
